b840285c3812376dae948dd86956522d14a53404
rexlin <rexlin6245@gmail.com>
2021-08-23 13:20:30 -0700

    debug deleteModal.js

--- public/javascripts/deleteModal.js
+++ public/javascripts/deleteModal.js
@@ -1,17 +1,17 @@
 const restaurantsList = document.querySelector('.restaurants-list')
-const myModal = new bootstrap.Modal(document.querySelector('.modalBox'))
-const form = document.querySelector('#delete-submit')
 
 if (restaurantsList) {
+  const myModal = new bootstrap.Modal(document.querySelector('.modalBox'))
+  const form = document.querySelector('#delete-submit')
+  const modalFooter = document.querySelector('.modal-footer')
   restaurantsList.addEventListener('click', event => {
     if (event.target.id === 'delete-btn') {
       const id = event.target.dataset.id
       form.action = `/admin/restaurants/${id}?_method=DELETE`
       myModal.show()
-      form.addEventListener('click', event => {
+      modalFooter.addEventListener('click', event => {
         if (event.target.classList.contains('delete-confirm') || event.target.classList.contains('delete-giveup')) {
           myModal.hide()
-          myModal.dispose()
         }
       })
     }
