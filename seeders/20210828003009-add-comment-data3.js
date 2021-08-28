c4cc55f9c357cb8f3eb113d4b301233570f14f1a
rexlin <rexlin6245@gmail.com>
2021-08-27 17:31:10 -0700

    add comment seed data

---
+++ seeders/20210828003009-add-comment-data3.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Comments',
+      Array.from({ length: 2 }).map((d, i) =>
+      ({
+        text: faker.lorem.text(),
+        UserId: 125,
+        RestaurantId: 1025,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      ), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
